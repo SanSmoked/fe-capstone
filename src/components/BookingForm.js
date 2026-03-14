77d9fc73881029203b13fecc85347791c836c125
Jose Aristizabal <j.aristizabal@astounddigital.com>
2026-03-13 23:52:54 -0500

    form style addded

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,3 +1,5 @@
+import './BookingForm.css';
+import CallToAction from './CallToAction';
 import { useState } from 'react';
 
 function BookingForm(props) {
@@ -26,67 +28,84 @@ function BookingForm(props) {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <h1>Book your table</h1>
-            <label htmlFor="res-date">Choose date</label>
-            <input
-                type="date"
-                id="res-date"
-                placeholder="Date"
-                name="date"
-                value={date}
-                min={today}
-                onChange={(e => {
-                    setDate(e.target.value);
-                    props.updateTimes({
-                        type: 'update_times',
-                        date: e.target.value
-                    })
-                })}
-                required
-            />
-            <label htmlFor="res-time">Choose time</label>
-            <select
-                id="res-time"
-                placeholder="time"
-                name="time"
-                value={time}
-                onChange={(e => {
-                    setTime(e.target.value);
-                })}
-            >
-                {props.availableTimes.map((time)=> (
-                    <option
-                    key={time}
-                    value={time}>
-                        {time}
-                    </option>
-                ))}
-            </select>
-            <label htmlFor="guests">Number of guests</label>
-            <input
-                type="number"
-                placeholder="1"
-                min="1"
-                max="10"
-                id="guests"
-                name="guests"
-                value={guests}
-                onChange={(e => setGuests(e.target.value))}
-            />
-            <label htmlFor="occasion">Occasion</label>
-            <select
-                id="occasion"
-                placeholder="occasion"
-                name="occasion"
-                value={occasion}
-                onChange={(e => setOccasion(e.target.value))}
-            >
-                <option value="birthday">Birthday</option>
-                <option value="anniversary">Anniversary</option>
-            </select>
-            <input type="submit" aria-label="Book your table" value="Make Your reservation" />
-        </form>
+        <section className="padding-elements">
+            <h1 className="bookingForm-title">Book your table</h1>
+            <form onSubmit={handleSubmit} className="bookingForm-section">
+                <section className="bookingForm-date bookingForm-inputs">
+                    <label htmlFor="res-date">Choose date</label>
+                    <input
+                        type="date"
+                        id="res-date"
+                        placeholder="Date"
+                        name="date"
+                        value={date}
+                        min={today}
+                        onChange={(e => {
+                            setDate(e.target.value);
+                            props.updateTimes({
+                                type: 'update_times',
+                                date: e.target.value
+                            })
+                        })}
+                        required
+                    />
+                </section>
+
+                <section className="bookingForm-time bookingForm-inputs">
+                    <label htmlFor="res-time">Choose time</label>
+                    <select
+                        id="res-time"
+                        placeholder="time"
+                        name="time"
+                        value={time}
+                        onChange={(e => {
+                            setTime(e.target.value);
+                        })}
+                    >
+                        {props.availableTimes.map((time)=> (
+                            <option
+                            key={time}
+                            value={time}>
+                                {time}
+                            </option>
+                        ))}
+                    </select>
+                </section>
+
+                <section className="bookingForm-guests bookingForm-inputs">
+                    <label htmlFor="guests">Number of guests</label>
+                    <input
+                        type="number"
+                        placeholder="1"
+                        min="1"
+                        max="10"
+                        id="guests"
+                        name="guests"
+                        value={guests}
+                        onChange={(e => setGuests(e.target.value))}
+                    />
+                </section>
+
+                <section className="bookingForm-occasion bookingForm-inputs">
+                    <label htmlFor="occasion">Occasion</label>
+                    <select
+                        id="occasion"
+                        placeholder="occasion"
+                        name="occasion"
+                        value={occasion}
+                        onChange={(e => setOccasion(e.target.value))}
+                    >
+                        <option value="birthday">Birthday</option>
+                        <option value="anniversary">Anniversary</option>
+                    </select>
+                </section>
+                <section className="bookingForm-submit">
+                    <button type="submit" aria-label="Book your table" value="Make Your reservation">
+                        <p>Make Your reservation</p>
+                    </button>
+                </section>
+            </form>
+        </section>
     );
 
 }
